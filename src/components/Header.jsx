44b3b53f3f67de4361b2ca62f8e85561d4b9066a
Fernando Pardo <thecrazypardo@gmail.com>
2025-12-16 19:25:47 +0100

    .env.local config

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+
+export default function Header() {
+  return (
+    <nav>
+      <Link href="/">Home</Link>
+      <Link href="/dashboard">Dashboard</Link>
+      <Link href="/about">About</Link>
+    </nav>
+  )
+}
